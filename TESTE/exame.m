3c5626f7ff5c7b8cd9b7816de86080db5d117884
monteiroo21 <joaopfmonteiro@gmail.com>
2023-12-27 18:57:11 +0000

    .

--- TESTE/exame.m
+++ TESTE/exame.m
@@ -3,4 +3,7 @@ numMovies = height(movies);
 
 genres = unique(movies(:,3));
 
-fprintf("Genres:\t%20s\t%8s\t%10s\t%10s\n\t\t%20s\t%8s\t%10s\t%10s\n\t%20s\t%8s\t%10s\t%10s\n\t%20s\t%8s\t%10s\t%10s\n\t%20s\t%8s\t%10s\t%10s\n", genres{1:20});
+fprintf(['Genres:\t%-16s\t%-8s\t%-10s\t%-10s\n\t\t%-16s\t%-8s\t%-10s\t%-10s' ...
+        '\n\t\t%-16s\t%-8s\t%-10s\t%-10s\n\t\t%-16s\t%-8s\t%-10s\t%-10s\n\t' ...
+        '\t%-16s\t%-8s\t%-10s\t%-10s\n'], genres{1:20});
+
